18ce779c5cb00ed953d0939c04858f937e2a6079
EricXx-Icaria <enavarros@instituticaria.cat>
2024-10-17 09:37:44 +0200

    divisio entera

--- diviso.py
+++ diviso.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 '''Bones practiques. divisió entre nombres enters
 Institut Icària
 Programació - 1r Batxillerat - Curs 2023-24
-Aquest programa et demana dos nombres enter,
+Aquest programa et demana dos nombres enters,
 després automàticament et fa la divisió
 i et surt el residu i el quocient
 '''
+
 __author__ = "Eric Navarro"
 __email__ = "enavarros@instituticaria.cat"
 __date__ = "2024/10/17"
